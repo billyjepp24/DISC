710b2e689beacff67b312e93acc00a2e7bd06de0
noc ojt <noc.ojt@onemagellan.com>
2025-07-18 14:33:05 +0800

    modify home

--- app/Http/Controllers/ApplicantsController.php
+++ app/Http/Controllers/ApplicantsController.php
@@ -13,7 +13,7 @@ class ApplicantsController extends Controller
      */
     public function index()
     {
-        return view('googleform-app');    
+        return view('applicants');
      }
 
     /**
